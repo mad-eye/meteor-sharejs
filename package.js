c30ef0765177fdbd2fdff0180de9c176edd5a3ae
Mike Risse <rissem@gmail.com>
2012-11-23 18:27:41 -0800

    files to render ace in the broswer connected to sharejs

---
+++ package.js
@@ -0,0 +1,10 @@
+Package.describe({
+  summary: "js files needed for a sharejs-based ace editor"
+});
+
+Package.on_use(function (api, where) {
+  api.add_files(['ace.js'], "client")
+  api.add_files(['bcsocket.js'], "client")
+  api.add_files(['share.uncompressed.js'], "client")
+  api.add_files(['share-ace.js'], "client")
+});
