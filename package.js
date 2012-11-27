6906a3ebc2b0e190a6c3966723f4335d8c05db83
Mike Risse <rissem@gmail.com>
2012-11-27 14:12:34 -0800

    use an uncompressed version of bcsocket

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
 
 Package.on_use(function (api, where) {
   api.add_files(['ace.js'], "client")
-  api.add_files(['bcsocket.js'], "client")
+  api.add_files(['bcsocket-uncompressed.js'], "client")
   api.add_files(['share.uncompressed.js'], "client")
   api.add_files(['share-ace.js'], "client")
 });
