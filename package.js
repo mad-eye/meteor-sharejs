c6fb6b630f8799734d8374a334b49b9094bd9eef
rissem <rissem@gmail.com>
2013-02-13 11:05:27 -0800

    upgrade to newer version of sharejs, remove some unused files

--- package.js
+++ package.js
@@ -3,8 +3,12 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
+  //it'd be nice to keep ace separate from sharejs and not include it her
+  //but ace has to be loaded before share-ace.js can run
   api.add_files(['ace.js'], "client")
   api.add_files(['bcsocket-uncompressed.js'], "client")
-  api.add_files(['share.uncompressed.js'], "client")
+  api.add_files(['share-uncompressed.js'], "client")
   api.add_files(['share-ace.js'], "client")
+  api.add_files(['share-json.js'], "client")
+  api.add_files(['share-text2-uncompressed.js'], "client")
 });
