4e4f8566f250033147f87a5c5e0f4ed53f67f9a0
rissem <rissem@gmail.com>
2013-02-21 14:47:57 -0800

    configure ace to look in /ace directory

---
+++ ace-config.js
@@ -0,0 +1,5 @@
+ace.config.set("modePath", "/ace");
+ace.config.set("themePath", "/ace")
+ace.config.set("workerPath", "/ace")
+
+
--- package.js
+++ package.js
@@ -6,6 +6,7 @@ Package.on_use(function (api, where) {
   //it'd be nice to keep ace separate from sharejs and not include it her
   //but ace has to be loaded before share-ace.js can run
   api.add_files(['ace.js'], "client")
+  api.add_files(['ace-config.js'], "client")
   api.add_files(['bcsocket-uncompressed.js'], "client")
   api.add_files(['share-uncompressed.js'], "client")
   api.add_files(['share-ace.js'], "client")
