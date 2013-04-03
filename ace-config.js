a5653600b46b46d4bfc301737536fe7f4f9ec899
James Gill <jamesagill@gmail.com>
2013-04-03 11:56:41 -0700

    Setting basePath to /ace, which allows search.

--- ace-config.js
+++ ace-config.js
@@ -1,5 +1,5 @@
 ace.config.set("modePath", "/ace");
-ace.config.set("themePath", "/ace")
-ace.config.set("workerPath", "/ace")
-
+ace.config.set("themePath", "/ace");
+ace.config.set("workerPath", "/ace");
+ace.config.set("basePath", "/ace");
 
